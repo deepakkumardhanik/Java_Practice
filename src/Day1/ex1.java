89ff05e6bdf95ced249d5e9c2d9f234ed89f59b3
coderdeepakkumar <deepakkumar07apr1999@gmail.com>
2024-07-20 12:47:21 +0530

    Trial commit

--- src/Day1/ex1.java
+++ src/Day1/ex1.java
@@ -1,2 +1,25 @@
-package Day1;public class ex1 {
+package Day1;
+
+public class ex1 {
+    //source code
+    public static void main(String[] args) {
+     //JavaC convert java (source code) to class code
+        System.out.println("Hello"); //JVM convert class to native code
+        System.out.println("Hello World!");
+        System.out.println("Hello World Java!!");
+
+        // Single Comment - This code will not run or executed
+        /**
+         * Multiple Comment
+         * Documentation Purpose
+         */
+
+        // Package -> Class -> Main Method -> statements(instructions)
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(3%3); // Integer
+
+    }
 }
